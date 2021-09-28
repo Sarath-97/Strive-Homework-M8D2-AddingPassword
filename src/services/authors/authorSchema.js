72e7c223c64a8d107adac3297408c60e261579aa
Sarath-97 <sarath559709@gmail.com>
2021-09-28 16:36:43 +0100

    chnages

--- src/services/authors/authorSchema.js
+++ src/services/authors/authorSchema.js
@@ -6,8 +6,9 @@ const { Schema, model } = mongoose
 
 const AuthorSchema = new Schema(
   {
-    name: { type: String, required: true },
+    firstname: { type: String, required: true },
     surname: { type: String, required: true },
+    email: { type: String, required: true },
     password: { type: String, required: true }
   },
   { timestamps: true }
